b44dbff21b391925fff214b2af503207dd33b403
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-01-25 20:37:43 +0600

    api issue

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qlklf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://sectors-involvement:gq9JmUTYehy*!B8@cluster0.qlklf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
